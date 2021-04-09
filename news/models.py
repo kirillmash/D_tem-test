83c82376ed474c2f813a6d458d9c1c4051671042
kirill <mashenkovkirill@gmail.com>
2021-04-09 11:32:15 +0300

    add NEWS model

---
+++ news/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
